9b9bf27e377cd4b7e1e65acd5389474ed6bbed9b
s-li1 <stevenli8892@hotmail.com.au>
2021-07-23 22:23:37 +1000

    css styling

--- components/Container.tsx
+++ components/Container.tsx
@@ -17,7 +17,7 @@ export default function Container(props) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <nav className="flex items-center justify-between w-full max-w-7xl p-8 mx-auto my-0 text-gray-900 bg-white sticky-nav md:my-8">
-                <div className="title-underline text-3xl">
+                <div className="link-underline text-3xl">
                     Steven Li.
                 </div>
                 <div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,26 +3,32 @@ import Image from 'next/image'
 import React from 'react'
 import Container from '../components/Container'
 import styles from '../styles/Home.module.css'
+import Link from 'next/dist/client/link'
 
 export default function Home() {
   return (
     <Container>
       <div className="flex flex-col justify-center items-center mx-auto mb-10 text-main-black">
-        <div className="flex w-3/4 justify-evenly md:w-full">
-          <div className="flex flex-col justify-center font-custom font-medium">
+        <div className="px-5 flex flex-col justify-evenly md:flex-row w-full p-12">
+          <div className="flex flex-col justify-center font-custom font-medium items-start mb-8">
             <h1 className="text-7xl font-semibold">
               Steven Li.
             </h1>
-            <h2 className="text-4xl text-gray-500 mb-16">
+            <h2 className="text-4xl text-gray-400 mb-6">
               Welcome to my world!
             </h2>
             <h3 className="text-3xl">
               I am an aspiring Software Engineer
             </h3>
-            <h3 className="text-3xl">based in Sydney, Australia.</h3>
+            <h3 className="text-3xl mb-8">based in Sydney, Australia.</h3>
+            <Link href="/about">
+              <a className="text-main-blue text-2xl font-normal rounded border-2 border-main-blue p-3 transform transition duration-500 hover:scale-110">
+                About Me
+              </a>
+            </Link>
           </div>
           <div>
-            <img src="/profile.jpeg" alt="Steven Li - Profile" width="350" height="350" className="rounded-full border-4 border-main-blue"/>
+            <img src="/profile.jpeg" alt="Steven Li - Profile" width="325" height="325" className="rounded-full border-4 border-main-blue m-3"/>
           </div>
         </div>
       </div>
