1e87c8b2b39a945bc7c59d367f2cdf0723938399
s-li1 <stevenli8892@hotmail.com.au>
2021-08-01 10:16:07 +1000

    social media links open new window

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -4,9 +4,9 @@ import { FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa';
 export default function SocialLinks() {
     return (
         <div className="flex md:flex-col gap-4">
-            <a href="https://github.com/s-li1"><FaGithub size="50"/></a>
-            <a href="https://www.linkedin.com/in/stevenlii/"><FaLinkedin size="50"/></a>
-            <a href="https://www.instagram.com/steeven_lee/"><FaInstagram size="50"/></a>
+            <a href="https://github.com/s-li1" target="_blank"><FaGithub size="50"/></a>
+            <a href="https://www.linkedin.com/in/stevenlii/" target="_blank"><FaLinkedin size="50"/></a>
+            <a href="https://www.instagram.com/steeven_lee/" target="_blank"><FaInstagram size="50"/></a>
         </div>
     )
 }
