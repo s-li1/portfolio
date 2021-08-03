43a4987d7c0b3735b9243f44b02d1ecdac94b939
s-li1 <stevenli8892@hotmail.com.au>
2021-08-03 12:31:14 +1000

    basic about page

--- components/Container.tsx
+++ components/Container.tsx
@@ -16,7 +16,7 @@ export default function Container(props) {
                 <meta name="description" content={meta.description} />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <nav className="flex items-center justify-between w-full max-w-7xl p-8 mx-auto my-0 text-gray-900 bg-white sticky-nav md:my-8">
+            <nav className="flex items-center justify-between w-full p-8 mx-auto my-0 text-gray-900 bg-white sticky-nav md:my-8">
                <Navigation/>
             </nav>
             <main>
--- pages/about.tsx
+++ pages/about.tsx
@@ -4,7 +4,33 @@ import Container from '../components/Container';
 export default function About() {
     return (
         <Container>
-
+            <div className="flex-col text-main-black p-10 md:flex md: items-center">
+                <div className="mb-20 md:flex">
+                    <div>
+                        <h1 className="text-4xl font-semibold mb-8">
+                            <span><img src="/animations/code.gif" alt="Code" width="65" height="65"/></span>About me.
+                        </h1>
+                        <h2 className="text-2xl max-w-prose mb-8">
+                            I’m a 2nd year student studying a Bachelor of Information Technology at the University of Technology Sydney.
+                        </h2>
+                        <h2 className="text-2xl max-w-prose mb-8">
+                            I’ve always had a passion for technology and seek to build impactful software with a focus on writing clean code.
+                        </h2>
+                    </div>
+                    <img className="h-full w-full md:max-w-md" src="/bit_cohort.jpeg" alt="Bachelor of Information Technology Cohort 2022"/>
+                </div>
+                <div className="mb-10 md:flex md:flex-row-reverse">
+                    <div className="md:ml-10">
+                        <h1 className="text-4xl font-semibold mb-8">
+                            <span><img src="/animations/globe.gif" alt="Globe" width="65" height="65"/></span>Language Learner.
+                        </h1>
+                        <h2 className="text-2xl max-w-prose mb-8">
+                            Beyond coding, I love improving my cross-cultural understanding through getting involved in extra-curriculars and learning languages. You can find me either dabbling in Mandarin or Japanese!
+                        </h2>
+                    </div>
+                    <img className="h-full w-full md:max-w-md" src="/acss.png" alt="Australia Chinese Students Society"/>
+                </div>
+            </div>
         </Container>
     )
 }
