3b9b80701080452c05172fdccde333e17e38af33
s-li1 <stevenli8892@hotmail.com.au>
2021-08-03 11:08:14 +1000

    center social links

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -3,7 +3,7 @@ import { FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa';
 
 export default function SocialLinks() {
     return (
-        <div className="flex md:flex-col gap-4">
+        <div className="flex gap-8 md:flex-col md: place-content-center">
             <a href="https://github.com/s-li1" target="_blank"><FaGithub size="50"/></a>
             <a href="https://www.linkedin.com/in/stevenlii/" target="_blank"><FaLinkedin size="50"/></a>
             <a href="https://www.instagram.com/steeven_lee/" target="_blank"><FaInstagram size="50"/></a>
