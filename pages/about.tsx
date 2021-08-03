ed43644ccda61468ba1fae66b285762474c597ec
s-li1 <stevenli8892@hotmail.com.au>
2021-08-03 12:48:31 +1000

    animated icons vertically aligned with headers

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,7 +8,7 @@ export default function About() {
                 <div className="mb-20 md:flex">
                     <div>
                         <h1 className="text-4xl font-semibold mb-8">
-                            <span><img src="/animations/code.gif" alt="Code" width="65" height="65"/></span>About me.
+                            <span className="inline-block align-middle"><img src="/animations/code.gif" alt="Code" width="65" height="65"/></span> About me.
                         </h1>
                         <h2 className="text-2xl max-w-prose mb-8">
                             I’m a 2nd year student studying a Bachelor of Information Technology at the University of Technology Sydney.
@@ -22,7 +22,7 @@ export default function About() {
                 <div className="mb-10 md:flex md:flex-row-reverse">
                     <div className="md:ml-10">
                         <h1 className="text-4xl font-semibold mb-8">
-                            <span><img src="/animations/globe.gif" alt="Globe" width="65" height="65"/></span>Language Learner.
+                            <span className="inline-block align-middle"><img src="/animations/globe.gif" alt="Globe" width="65" height="65"/></span> Language Learner.
                         </h1>
                         <h2 className="text-2xl max-w-prose mb-8">
                             Beyond coding, I love improving my cross-cultural understanding through getting involved in extra-curriculars and learning languages. You can find me either dabbling in Mandarin or Japanese!
