ea200cddc8e74534ad93467a70b312f81c0d9fab
s-li1 <stevenli8892@hotmail.com.au>
2021-08-06 09:19:20 +1000

    hover animation for blog cards

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -9,7 +9,7 @@ export default function Blog() {
                 <h1 className="text-4xl font-semibold mb-8 p-8 self-center text-center">My Blog.</h1>
                 <div className="p-10 md:flex md:justify-around">
                     <Link href="blog/personal">
-                    <div className="max-w-lg cursor-pointer">
+                    <div className="max-w-lg cursor-pointer transform transition duration-500 hover:scale-105">
                         <img className="w-full" src="/blog/personal/personal.jpg" alt="Personal Writing"/>
                         <div className="px-6 py-4">
                             <h3 className="text-2xl font-semibold mb-2">
@@ -20,7 +20,7 @@ export default function Blog() {
                     </div>
                     </Link>
                     <Link href="blog/technical">
-                        <div className="max-w-lg cursor-pointer">
+                        <div className="max-w-lg cursor-pointer transform transition duration-500 hover:scale-105">
                             <img className="w-full" src="/blog/technical/technical.png" alt="Technical Technical"/>
                             <div className="px-6 py-4">
                                 <h3 className="text-2xl font-semibold mb-2">Technical</h3>
