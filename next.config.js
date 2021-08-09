5b6fb8089bb0c91c13fe7cbae135c9c9a47b30d8
s-li1 <stevenli8892@hotmail.com.au>
2021-08-10 07:26:12 +1000

    removed react strict mode config

--- next.config.js
+++ next.config.js
@@ -4,8 +4,4 @@ const withMDX = require('@next/mdx')({
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx']
-})
-
-module.exports = {
-  reactStrictMode: true,
-}
+});
