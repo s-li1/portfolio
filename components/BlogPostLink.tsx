787ea7420ab59cad01a847b03fc0d840083e509d
s-li1 <stevenli8892@hotmail.com.au>
2021-08-15 09:22:40 +1000

    added link tag to blog post link

--- components/BlogPostLink.tsx
+++ components/BlogPostLink.tsx
@@ -1,14 +1,19 @@
+import Link from 'next/link';
 import React from 'react';
 import { IPost } from '../types/types';
 
 export default function BlogPostLink(post: IPost) {
     return (
-        <div className="border-b-2 border-main-gray mb-8 pb-8 md:flex">
-            <span className="text-lg md:mr-5 text-main-blue">{post.date}</span>
-            <div>
-                <h3 className="text-2xl font-semibold pb-5">{post.title}</h3>
-                <p className="text-lg">{post.summary}</p>
-            </div>
-        </div>
+        <Link href="/">
+            <a>
+                <div className="border-b-2 border-main-gray mb-8 pb-8 md:flex">
+                    <span className="text-lg md:mr-5 text-main-blue">{post.date}</span>
+                    <div>
+                        <h3 className="text-2xl font-semibold pb-5">{post.title}</h3>
+                        <p className="text-lg">{post.summary}</p>
+                    </div>
+                </div>
+            </a>
+        </Link>
     )
 }
