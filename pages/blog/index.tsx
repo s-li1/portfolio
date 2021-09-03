e5605d94f04cf0efa0abb501c3c86ab2a618e4e3
s-li1 <stevenli8892@hotmail.com.au>
2021-09-03 13:28:03 +1000

    center blog section and even spacing on large screens

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -9,38 +9,40 @@ import Document from '../../public/animations/blog.gif';
 export default function Blog() {
     return (
         <Container title="Blog - Steven Li" description="Sharing my journey through life and programming">
-            <div>
-                <h1 className="text-4xl font-semibold p-10 self-center text-center">
-                    <span className="inline-block align-middle">
-                        <Image src={Document} alt="Code" width="65" height="65"/>
-                    </span> 
-                    My Blog.
-                </h1>
-                <div className="px-10 md:flex md:justify-evenly">
-                    <Link href="blog/personal" passHref>
-                    <a className="max-w-lg cursor-pointer transform transition duration-500 hover:scale-105 mb-8">
-                        <div className="w-full">
-                            <Image src={Personal} alt="Personal Writing"/>
-                        </div>
-                        <div className="px-6 py-4">
-                            <h3 className="text-2xl font-semibold mb-2">
-                                Personal
-                            </h3>
-                            <p className="text-lg">Sharing my journey through life.</p>
-                        </div>
-                    </a>
-                    </Link>
-                    <Link href="blog/technical" passHref>
-                        <a className="max-w-lg cursor-pointer transform transition duration-500 hover:scale-105">
-                            <div className="w-full">
-                                <Image src={Technical} alt="Technical Writing"/>
-                            </div>
-                            <div className="px-6 py-4">
-                                <h3 className="text-2xl font-semibold mb-2">Technical</h3>
-                                <p className="text-lg">Sharing my learnings as a developer.</p>
-                            </div>
-                        </a>
-                    </Link>
+            <div className="text-center md:flex md:justify-center md:items-center md:absolute md:inset-0">
+                <div>
+                    <h1 className="text-4xl font-semibold p-10 self-center text-center">
+                        <span className="inline-block align-middle">
+                            <Image src={Document} alt="Code" width="65" height="65"/>
+                        </span> 
+                        My Blog.
+                    </h1>
+                    <div className="px-10 space-x-20 md:flex md:justify-center">
+                        <Link href="blog/personal" passHref>
+                            <a className="max-w-lg cursor-pointer transform transition duration-500 hover:scale-105 mb-8">
+                                <div className="w-full">
+                                    <Image src={Personal} alt="Personal Writing"/>
+                                </div>
+                                <div className="py-4">
+                                    <h3 className="text-2xl font-semibold mb-2">
+                                        Personal
+                                    </h3>
+                                    <p className="text-lg">Sharing my journey through life.</p>
+                                </div>
+                            </a>
+                        </Link>
+                        <Link href="blog/technical" passHref>
+                            <a className="max-w-lg cursor-pointer transform transition duration-500 hover:scale-105">
+                                <div className="w-full">
+                                    <Image src={Technical} alt="Technical Writing"/>
+                                </div>
+                                <div className="py-4">
+                                    <h3 className="text-2xl font-semibold mb-2">Technical</h3>
+                                    <p className="text-lg">Sharing my learnings as a developer.</p>
+                                </div>
+                            </a>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </Container>
