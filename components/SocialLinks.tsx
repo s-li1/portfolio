271c2a5d6649014f53e2f7bcd9a786c130df87f7
s-li1 <stevenli8892@hotmail.com.au>
2021-09-03 13:50:54 +1000

    replace gap with space between css for browser support

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 import { FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa';
 
 export default function SocialLinks() {
     return (
-        <div className="flex gap-8 p-8 md:flex-col md:place-content-center">
+        <div className="m-8 p-8 flex space-x-9 md:flex-col md:space-y-8 md:space-x-0">
             <a href="https://github.com/s-li1" target="_blank" rel="noreferrer" className="transform transition duration-300 hover:text-main-blue"><FaGithub size="50"/></a>
             <a href="https://www.linkedin.com/in/stevenlii/" target="_blank" rel="noreferrer" className="transform transition duration-300  hover:text-main-blue"><FaLinkedin size="50"/></a>
             <a href="https://www.instagram.com/steeven_lee/" target="_blank" rel="noreferrer" className="transform transition duration-300 hover:text-main-blue"><FaInstagram size="50"/></a>
