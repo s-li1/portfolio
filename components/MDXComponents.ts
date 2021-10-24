cc0b5ea4718412878e0244f3c44ad62f07f2e5fd
s-li1 <stevenli8892@hotmail.com.au>
2021-10-24 23:47:28 +1100

    add animatedimage as mdxcomponent

--- components/MDXComponents.ts
+++ components/MDXComponents.ts
@@ -1,7 +1,9 @@
 import Image from 'next/image';
+import AnimatedImage from './AnimatedImage';
 
 const MDXComponents = {
-    Image: Image
+    Image: Image,
+    AnimatedImage: AnimatedImage
 }
 
 export default MDXComponents;
