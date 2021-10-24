2b44767da0e3f8abd628058800a043bef457fe3a
s-li1 <stevenli8892@hotmail.com.au>
2021-10-24 23:45:44 +1100

    create animate image

---
+++ components/AnimatedImage.tsx
@@ -0,0 +1,35 @@
+import { once } from "events";
+import { motion } from "framer-motion";
+import Profile from '../public/profile.jpeg';
+import Image from "next/dist/client/image";
+import { useState } from "react";
+
+export default function AnimatedImage(props) {
+    const { src, alt, ...imageSize } = props;
+    const [imageLoading, setImageLoading] = useState(true);
+    const [pulsing, setPulsing] = useState(true);
+
+    const imageLoaded = () => {
+        setImageLoading(false);
+        setTimeout(() => setPulsing(false), 800);
+    }
+    
+    return (
+        <div className={`${pulsing ? "animate-pulse" : "" } bg-main-gray`}>
+            <motion.div
+                initial={{ height: "1rem", opacity: 0 }}
+                animate={{
+                    height: imageLoading ? "1rem" : "auto",
+                    opacity: imageLoading ? 0 : 1
+                }}
+                transition={{
+                    height: { delay: 0, duration: 0.5 },
+                    opacity: { delay: 0.5, duration: 0.5 }
+                }}
+                onLoad={imageLoaded}
+            >
+                <Image src={src} alt={alt} {...imageSize} />
+            </motion.div>
+        </div>
+      );
+}
